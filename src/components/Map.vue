94fe8d1d3b2cd4b445b9d26c70d1a48b6ede67dc
xiequan <1061473663@qq.com>
2021-02-04 11:15:48 +0800

    vue + cesium 结合初始化

---
+++ src/components/Map.vue
@@ -0,0 +1,36 @@
+<template>
+	<div id="container" class="box">
+		<div id="cesiumContainer"></div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Map',
+	data() {
+		return {}
+	},
+	mounted() {
+		this.initMap()
+	},
+	methods: {
+		initMap() {
+			let Cesium = this.cesium
+            let viewer = new Cesium.Viewer('cesiumContainer')
+            viewer._cesiumWidget._creditContainer.style.display = "none";// 隐藏版权
+		},
+	},
+}
+</script>
+
+<style scope>
+#cesiumContainer {
+	width: 100vw;
+    height: 100vh;
+	overflow: hidden;
+}
+.box {
+	width: 100vw;
+    height: 100vh;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+//引入cesium相关文件
+var cesium = require('cesium/Cesium')
+var widgets= require('cesium/Widgets/widgets.css')
+ 
+Vue.prototype.cesium = cesium
+Vue.prototype.widgets = widgets
+
+
 Vue.config.productionTip = false
 
 new Vue({
